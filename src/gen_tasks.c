c733cd0df62dbd447590b9b58bd3a268dc230a5e
VitaminOh <dlin01@stuy.edu>
2021-09-21 15:23:43 -0400

    changed formatting

--- src/gen_tasks.c
+++ src/gen_tasks.c
@@ -3,9 +3,9 @@
 
 void main() {
   srand(10132002);
-  FILE *fp = fopen("tasks.txt", "w");
+  FILE *fp = fopen("tasks.txt", "w+");
   for (int i = 1; i <= 100; i++) {
-    fprintf(fp, "prio=%d burst=%d arrivalDelay=%d\n", rand()%256, 1+rand()%100, 1+rand()%10);
+    fprintf(fp, "%d %d %d\n", rand()%256, 1+rand()%100, 1+rand()%10);
   }
   fclose(fp);
 }
