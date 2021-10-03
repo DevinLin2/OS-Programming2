1a1624d13f8c1deafce2508d7dcb0b324a4a330e
VitaminOh <dlin01@stuy.edu>
2021-10-03 16:53:57 -0400

    determined whether or not pages are allocated by some fixed expansion size

--- src/question4/lazy.c
+++ src/question4/lazy.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 void main() {
+  // test for lazy allocaion
   struct timespec ts;
   double before, after;
   int *ptr = (int*)malloc(10 * sizeof(int));
@@ -24,4 +25,27 @@ void main() {
   timespec_get(&ts, TIME_UTC);
   after = ts.tv_nsec;
   printf("Time used for third write in nanoseconds: %f\n", after - before);
+  // test for if malloc has a fixed heap expansion size
+  int *array1 = (int*)malloc(10*sizeof(int));
+  for (int i = 0; i < 10; i++) {
+    array1[i] = i;
+  }
+  int *array2 = (int*)malloc(30*sizeof(int));
+  for (int i = 0; i < 30; i++) {
+    array2[i] = i;
+  }
+  int array3[10];
+  for (int i = 0; i < 10; i++) {
+    array3[i] = i;
+  }
+  int array4[30];
+  for (int i = 0; i < 30; i++) {
+    array3[i] = i;
+  }
+  printf("Size of dynamically allocated arrays:\n");
+  printf("Size of first array of size 10 ints: %li\n", sizeof(array1));
+  printf("Size of first array of size 30 ints: %li\n", sizeof(array2));
+  printf("Size of statically allocated arrays:\n");
+  printf("Size of first array of size 10 ints: %li\n", sizeof(array3));
+  printf("Size of first array of size 30 ints: %li\n", sizeof(array4));
 }
